0a99ba5b7be572e2a5e97f443d4c34cc74988277
Greatbear <littlebeargreat@gmail.com>
2017-07-13 21:47:07 +0800

    多行省略

--- 多行省略/lue.js
+++ 多行省略/lue.js
@@ -24,9 +24,12 @@ $(function(){
 
 	/**
 	 *使用方法：
-	 *调用函数fierceEllipsis(className,n),
-	 *传入两个值：
+	 * 1. 依赖于jQuery，可以自己改成原生的
+	 * 2. 调用函数fierceEllipsis(className,n)
+	 * 3. 传入两个值：
 	 *				第一个值：要设置标签的class名（支持标签：p、span，，，，其它的自己去测试）
 	 *				第二个值：该标签下一共要显示的行数，数字格式
+	 * 4. 每设置一处地方就要调用一次 fierceEllipsis函数，
+	 *    为什么这样做？因为有可能所有要设置的地方要显示的行数不都是一样的
 	 */
 })
